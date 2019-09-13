cef142e2761bf3e6dc6ea39e8aae85de395161c2
Frank Corvey <jeff.thomas6@mail.dcu.ie>
2019-09-13 14:48:30 +0100

    Made new input method and needed global variables

--- attempt_2.py
+++ attempt_2.py
@@ -2,6 +2,24 @@
 import sys
 
 def main():
+    mainSoduko = {}
+    emptyPlaces = []
+    filledCells = []
+    colIndex = 0
+    rowIndex = 0 
+    inputLine = list(input().strip())
+    print(inputLine)
+    for rowIndex in range(0,9):
+        for colIndex in range(0,9):
+            num = inputLine[rowIndex *9 + colIndex]
+            index = str(rowIndex) + str(colIndex)
+            if (num == "0"):
+                mainSoduko[index] = set()
+                emptyPlaces.append(index)
+            else:
+                mainSoduko[index] = {int(num)}
+                filledCells.append(index)
+    print(mainSoduko)
 
 # def input():
 
